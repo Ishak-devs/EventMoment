79f874b789be59ddad37dad2b519506099d2f610
ishak <i.kouici@insta.fr>
2025-08-16 16:10:47 +0200

    code for connexion db almost okay

--- db.js
+++ db.js
@@ -8,4 +8,12 @@ const pool = mysql.createPool({
   database: 'eventmoment', 
 });
 
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.error('Erreur de connexion MySQL :', err.message);
+  } else {
+    console.log('Connexion MySQL OK');
+    connection.release();
+  }
+});
 module.exports = pool.promise();
